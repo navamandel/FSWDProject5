c8403db49b8b8bcf56169f5f3eac825d92f15166
navamandel <navanmandel@gmail.com>
2025-05-22 14:35:44 +0300

    Form component, fake jason server with some user information and a context for authentication/user

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,1 +1,45 @@
 // Form component to handle login, register, etc
+
+import { useState } from "react";
+
+// Template for all forms, so form login only has to be handled once
+// title: type of form, text to display at the top
+// inputs: input fields which include: label, type, name, placeholder
+// handleSubmit function from the parent component
+// buttonText: text to display on the submit button
+export default function Form({ title, inputs, handleSubmitData, buttonText }) {
+
+    const [formData, setFormData] = useState({})
+
+    function handleChange(e) {
+        e.preventDefault();
+
+        setFormData(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        handleSubmitData({...formData});
+    }
+
+    return (
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <h3>{title}</h3>
+            {inputs.map(({ label, type, name, placeholder }) => (
+                <section key={name}>
+                    <label>{label}</label>
+                    <input
+                        type={type}
+                        name={name}
+                        placeholder={placeholder}
+                        onChange={(e) => handleChange(e)}
+                    />
+                </section>
+            ))}
+            <button type="submit">{buttonText}</button>
+        </form>
+    );
+}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,51 @@
 // Form -> username, password, verify password
 // On submit -> check if username exsits
 // If not go to CompleteProfile.jsx
+import Form from '../components/Form.jsx'
+import { useAuth } from '../context/AuthContext.jsx';
+import { useNavigate } from 'react-router-dom';
+
+export default function Register() {
+
+    const { register } = useAuth();
+    const nav = useNavigate();
+
+    const handleSubmit = async (formData) => {
+        if (formData.password != formData.confirmpassword) {
+            alert("Passwords must match");
+        } else {
+            const { success } = await register(formData);
+            success ? nav('/completeprofile') : alert("Please choose a different username");
+        }
+    };
+    
+    return (
+        <>
+            <Form 
+                title="Register"
+                inputs={[
+                    { 
+                        label: "Username", 
+                        type: "text", 
+                        name: "username", 
+                        placeholder: "Choose Username"
+                    },
+                    {
+                        label: "Password", 
+                        type: "password", 
+                        name: "password", 
+                        placeholder: "Choose Password"
+                    },
+                    {
+                        label: "Confirm Password", 
+                        type: "password", 
+                        name: "confirmpassword", 
+                        placeholder: "Confirm Password"
+                    }
+                ]}
+                handleSubmitData={(formData) => handleSubmit(formData)}
+                buttonText="Complete Registration"
+            />
+        </>
+    );
+}
